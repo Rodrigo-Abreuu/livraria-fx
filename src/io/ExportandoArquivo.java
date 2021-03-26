30f0cac748488b93503544d2d38c488c094c175b
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-03-26 13:05:03 -0300

    Melhorando teste ExportandoArquivo

--- src/io/ExportandoArquivo.java
+++ src/io/ExportandoArquivo.java
@@ -1,22 +1,15 @@
 package io;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 
 public class ExportandoArquivo {
 	
 	public static void main(String[] args) throws IOException {
 			
-		OutputStream os = new FileOutputStream("Saida.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(os);
-		BufferedWriter bw = new BufferedWriter(osw);
-	
-		bw.write("Testando a escrita de um arquivo");
-		bw.newLine();
-		bw.write("Conteudo da proxima linha");
-		bw.close();
+		PrintStream out = new PrintStream("Saida.txt");
+		out.println("Testando a escrita de um arquivo");
+		out.println("Conteudo da proxima linha");
+		out.close();
 	}
 }
